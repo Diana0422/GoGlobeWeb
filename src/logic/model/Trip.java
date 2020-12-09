9b2fba65a2e3536a2b641ffbb9d6c8d0dea9275f
Diana0422 <diana.pasquali@students.uniroma2.eu>
2020-12-09 18:36:45 +0100

    Added methods in PersistenceController to save Trips to a back-end file (trips.txt).

---
+++ src/logic/model/Trip.java
@@ -0,0 +1,73 @@
+package logic.model;
+
+import java.awt.image.BufferedImage;
+import java.io.Serializable;
+
+public class Trip implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String title;
+	private int price;
+	private TripCategory category1;
+	private TripCategory category2;
+	private BufferedImage img;
+	
+	public Trip(int id, String title, String imgFilename, int price, TripCategory cat1, TripCategory cat2) {
+		this.id = id;
+		this.title = title;
+		this.price = price;
+		this.category1 = cat1;
+		this.category2 = cat2;	
+	}
+	
+	public BufferedImage getImg() {
+		return img;
+	}
+	
+	public void setImg(BufferedImage img) {
+		this.img = img;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public TripCategory getCategory1() {
+		return category1;
+	}
+	
+	public void setCategory1(TripCategory category1) {
+		this.category1 = category1;
+	}
+	
+	public TripCategory getCategory2() {
+		return category2;
+	}
+	
+	public void setCategory2(TripCategory category2) {
+		this.category2 = category2;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
