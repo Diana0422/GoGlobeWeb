4957b7e2a5deaf9d72f23e258955c664d7414b0b
lorenzo tanzi <lorenzo.tanzi997@gmail.com>
2020-12-13 22:57:08 +0100

    issue #34

---
+++ src/logic/model/Day.java
@@ -0,0 +1,34 @@
+package logic.model;
+
+import java.util.List;
+
+public class Day {
+	
+	private String name;
+	private List<String> locations;
+	String plan;
+	
+	public String getName() {
+		return name;
+		
+		
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public List<String> getLocations() {
+		return locations;
+	}
+	public void setLocations(List<String> locations) {
+		this.locations = locations;
+	}
+	public String getPlan() {
+		return plan;
+	}
+	public void setPlan(String plan) {
+		this.plan = plan;
+	}
+	
+}
