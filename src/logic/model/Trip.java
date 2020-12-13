0b5fe436506b8a120b5c500251c7e966619d7c5f
lorenzo tanzi <lorenzo.tanzi997@gmail.com>
2020-12-13 22:56:38 +0100

    issue #35

--- src/logic/model/Trip.java
+++ src/logic/model/Trip.java
@@ -2,18 +2,22 @@ package logic.model;
 
 import java.awt.image.BufferedImage;
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
 public class Trip implements Serializable {
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
+
 	private int id;
 	private String title;
 	private int price;
 	private TripCategory category1;
 	private TripCategory category2;
 	private BufferedImage img;
+	private Date departureDate; //write get and set
+	private Date returnDate;	//write get and set
+	private List<Day> days;	
 	
 	public Trip(int id, String title, String imgFilename, int price, TripCategory cat1, TripCategory cat2) {
 		this.id = id;
@@ -69,6 +73,14 @@ public class Trip implements Serializable {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+	
+	public List<Day> getDays() {
+		return days;
+	}
+
+	public void setDays(List<Day> days) {
+		this.days = days;
 	}
 
 }
