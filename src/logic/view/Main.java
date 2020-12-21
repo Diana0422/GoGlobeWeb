b4abc5c8aca17d35a7f686e079cd836a41b5c1ad
Diana0422 <daylight@live.it>
2020-12-21 12:25:22 +0100

    Issue #37: Try build adding file Main.java in view package.

---
+++ src/logic/view/Main.java
@@ -0,0 +1,26 @@
+package logic.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/logic/view/Registration.fxml"));
+			Scene scene = new Scene(root,1024,550);
+			scene.getStylesheets().addAll(getClass().getResource("/logic/view/css/application.css").toExternalForm(), getClass().getResource("/logic/view/css/registration.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
